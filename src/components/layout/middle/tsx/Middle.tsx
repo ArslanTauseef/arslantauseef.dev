41ad96bf3afa9d9a671a3061e66a62daa46b04f3
ArsalanTauseef <232355832+ArslanTauseef@users.noreply.github.com>
2026-01-04 19:40:30 +0500

    #11

--- src/components/layout/middle/tsx/Middle.tsx
+++ src/components/layout/middle/tsx/Middle.tsx
@@ -4,7 +4,11 @@ import MyPicture from "../../../../assets/Pictures/MyPicture/MyPicture.png"
 export const Middle = () => {
   return (
     <main className="at-main">
-        <section className="left-main-section"></section>
+        <section className="left-main-section">
+            <div><h1>Hi, this is a test heading.</h1></div>
+            <div><h2>This website is a personal Portfolio.</h2></div>
+            <div><h3>Thanks, for taking interest.</h3></div>
+        </section>
         <section className="right-main-section">
             <div><img src={MyPicture} alt="Profile Picture (Arslan Tauseef)" /></div>
         </section>
